e0bb326fded6eadc9e1b39e08add947e553aebc9
Eric Hendrickson <enhendrickson@gmail.com>
2019-10-27 11:33:59 -0500

    add kubernetes auth

---
+++ src/Environments/ConfigEnvironmentKubernetesVault.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CashExpress\ConfigSync\Environments;
+
+use Illuminate\Console\Command;
+use CashExpress\ConfigSync\Connections\ConnectionVault;
+
+class ConfigEnvironmentKubernetesVault extends ConfigEnvironmentBase
+{
+  private $environment;
+  private $config;
+
+  public function __construct($environment) {
+    $this->environment = $environment;
+    $this->config = config('config-sync.environments.' . $environment);
+  }
+
+  public function getEnvironmentConnection($credentials) : ConnectionVault {
+    $connectionVault = new ConnectionVault($this->config['base_uri']);
+    $connectionVault->connectByK8sJwt($credentials['jwt'], $credentials['role']);
+    return $connectionVault;
+  }
+
+  public function getConfig() {
+    return $this->config;
+  }
+
+}
